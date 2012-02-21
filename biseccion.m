e84fa5b1cebc88c02ae99dd02d1fddb1d7da3cc7
Leonardo Yamasaki <leoyamasaki@gmail.com>
2012-02-21 14:47:42 -0600

    Biseccion en java y ruby

--- biseccion.m
+++ biseccion.m
@@ -1,17 +1,21 @@
-function xr = biseccion(f, a, b, umbral = 0.000001)
-  if feval(f, a) * feval(f, b) >= 0
+function xr = biseccion(x0, x1, umbral = 0.0000000001)
+  if f(x0) * f(x1) >= 0
     error("No hay cambio de signo entre los extremos del intervalo.");
   end
   error = 1;
-  xr = a;
+  xr = x0;
   while error > umbral
     xa = xr;
-    xr = (a + b) / 2;
+    xr = (x0 + x1) / 2;
     error = abs((xr - xa) / xr);
-    if feval(f, a) * feval(f, xr) < 0
-      b = xr;
+    if f(x0) * f(xr) < 0
+      x1 = xr;
     else
-     a = xr;
+      x0 = xr;
     end
   end
+end
+
+function y = f(x)
+  y = sin(x);
 end
